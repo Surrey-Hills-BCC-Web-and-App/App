f942eacbe3f63db4891213b9d22f82b881826ee0
ethan-stebbins <62233164+ethan-stebbins@users.noreply.github.com>
2020-03-23 11:11:19 +1100

    App V1 Build 1

---
+++ Podcasts and News/Podcasts and News/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Podcasts and News
+//
+//  Created by Ethan Stebbins on 20/3/20.
+//  Copyright © 2020 Ethan Stebbins. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let webaddress = "http://127.0.0.1/WVPodcasts.php"
+        if let url = URL(string: webaddress) {
+            let urlRequest = URLRequest(url: url)
+            self.webView.load(urlRequest)
+        }
+    }
+
+
+}
---
+++ Podcasts and News/Podcasts and News/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Podcasts and News
+//
+//  Created by Ethan Stebbins on 20/3/20.
+//  Copyright © 2020 Ethan Stebbins. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let webaddress = "http://127.0.0.1/WVWhat's-On.php"
+        if let url = URL(string: webaddress) {
+            let urlRequest = URLRequest(url: url)
+            self.webView.load(urlRequest)
+        }
+    }
+
+
+}
